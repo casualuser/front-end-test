8be2b1f530b617d0bd97ad4bfd91ef3afe369d58
bernardstanislas <bernardstanislas@gmail.com>
2016-01-17 21:20:09 +0100

    Refactor

--- src/client/entry1.js
+++ src/client/entry1.js
@@ -1,32 +1,3 @@
-import {sourceId} from './sourceId';
-import peerChannels from './peerChannels';
-import {INIT} from '../messages';
-import messageHandler from './messageHandler';
-import {registerDomListeners} from './dom';
+import {initializer} from './init';
 
-// TODO : with more time available, organise the mess with Redux :)
-
-const webSockerURI = `ws://localhost:${__SERVER_PORT__}/`;
-
-document.addEventListener('DOMContentLoaded', () => {
-    registerDomListeners();
-    console.log(`%cI am ${sourceId}`, `color: #${sourceId.substr(0, 6)}; font-size: 2em; font-weight: bold`);
-    const webSocket = new WebSocket(webSockerURI);
-    webSocket.onopen = () => {
-        webSocket.send(JSON.stringify({
-            type: INIT,
-            source: sourceId,
-            question: 1
-        }));
-    };
-    webSocket.onmessage = ({data}) => {
-        const message = JSON.parse(data);
-        messageHandler(message, webSocket);
-    }
-
-    window.onbeforeunload = () => {
-        Object.keys(peerChannels).map(peerId => {
-            peerChannels[peerId].close();
-        });
-    }
-});
+initializer(1);
---
+++ src/client/init.js
@@ -0,0 +1,34 @@
+import {sourceId} from './sourceId';
+import peerChannels from './peerChannels';
+import {INIT} from '../messages';
+import messageHandler from './messageHandler';
+import {registerDomListeners} from './dom';
+
+// TODO : with more time available, organise the mess with Redux :)
+
+const webSockerURI = `ws://localhost:${__SERVER_PORT__}/`;
+
+export const initializer = question => {
+    document.addEventListener('DOMContentLoaded', () => {
+        registerDomListeners();
+        console.log(`%cI am ${sourceId}`, `color: #${sourceId.substr(0, 6)}; font-size: 2em; font-weight: bold`);
+        const webSocket = new WebSocket(webSockerURI);
+        webSocket.onopen = () => {
+            webSocket.send(JSON.stringify({
+                type: INIT,
+                source: sourceId,
+                question
+            }));
+        };
+        webSocket.onmessage = ({data}) => {
+            const message = JSON.parse(data);
+            messageHandler(message, webSocket);
+        }
+
+        window.onbeforeunload = () => {
+            Object.keys(peerChannels).map(peerId => {
+                peerChannels[peerId].close();
+            });
+        }
+    });
+}
